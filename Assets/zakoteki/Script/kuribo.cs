48c6bca45bf65c481720b39e4b40413a2696645d
namekitikin <namekitikin@icloud.com>
2024-07-22 04:09:36 +0900

    kuribo_syusei

--- Assets/zakoteki/Script/kuribo.cs
+++ Assets/zakoteki/Script/kuribo.cs
@@ -6,25 +6,27 @@ using UnityEngine;
  
 public class kuribo : MonoBehaviour
 {
+  public Rigidbody2D rid2d;
+ public float hanntenn;
+public float moveX;
      　　 private SpriteRenderer sr = null; 
      void Start()
      {
-
+        
      }
- public float hanntenn;
-public float moveX;
+     
      void Update()
      {
-      
-        transform.position = transform.position + new Vector3(moveX, 0, 0 ) ;
-      
+      rid2d.velocity = new Vector2(moveX,0);
+        //transform.position = transform.position + new Vector3(moveX, 0, 0 ) ;
+        
      }
-       void OnTriggerEnter (Collider butukattamono)
+       void OnCollisionEnter2D (Collision2D butukattamono)
        {  
         
          if(butukattamono.gameObject.tag == "Wall")
         {
-        moveX = moveX * hanntenn;
+          moveX=moveX*hanntenn;
         }
        }
 }
