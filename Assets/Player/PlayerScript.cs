da51a384682e5127b9c0903f132447ff3fab0854
nimparo-admin <ogretotem.game@gmail.com>
2024-07-21 16:24:25 +0900

    Player Move Camera Move

--- Assets/Player/PlayerScript.cs
+++ Assets/Player/PlayerScript.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 0.1f;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+        float posX, posY;
+
+        posX = transform.position.x;
+        posY = transform.position.y;
+
+        if (Input.GetKey(KeyCode.D) == true) {
+            posX += speed;
+        }
+
+        if (Input.GetKey(KeyCode.A) == true) {
+            posX -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.W) == true) {
+            posY += speed;
+        }
+
+        if (Input.GetKey(KeyCode.S) == true) {
+            posY -= speed;
+        }
+
+        transform.position = new Vector3(posX, posY, 0);
     }
 }
