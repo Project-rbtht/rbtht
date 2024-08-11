94c78e2ee0933c08e88476bc3007c19c24ee3885
nimparo-admin <ogretotem.game@gmail.com>
2024-08-11 20:42:15 +0900

    Player Animations Player Moves Player Animation improvement player collider improved player Attack collider added player Attack damage player HP player Attack recastTime

--- Assets/Player/PlayerScript.cs
+++ Assets/Player/PlayerScript.cs
@@ -1,35 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
     public float speed = 0.1f;
+    public int jpNumMax = 1;
+    public float jpSpeed = 9.8f;
+    public ground_judge groundJudge;
+
+    Rigidbody2D rb;
+    int jpNum;
+    Animator anim = null;
+
+    void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        jpNum = jpNumMax;
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update() {
 
-        float posX, posY;
+        float x = Input.GetAxisRaw("Horizontal");
+        float speedY = rb.velocity.y;
 
-        posX = transform.position.x;
-        posY = transform.position.y;
-
-        if (Input.GetKey(KeyCode.D) == true) {
-            posX += speed;
+        if (Input.GetButton("Walk") == true) {
+            x = x / 2;
         }
 
-        if (Input.GetKey(KeyCode.A) == true) {
-            posX -= speed;
+        if (speedY < 0 && anim.GetInteger("Jump") > 0) {
+            anim.SetInteger("Jump", -1);
         }
 
-        if (Input.GetKey(KeyCode.W) == true) {
-            posY += speed;
+        if (Input.GetButtonDown("Jump") == true) {
+            if (groundJudge.onGround == true) {
+                jpNum = jpNumMax;
+            }
+            if (jpNum > 0) {
+                speedY = jpSpeed;
+                if (groundJudge.onGround == true) {
+                    anim.SetInteger("Jump", 1);
+                }else{
+                    anim.SetInteger("Jump", 2);
+                }
+                jpNum--;
+            }
         }
 
-        if (Input.GetKey(KeyCode.S) == true) {
-            posY -= speed;
+        rb.velocity = new Vector2(x * speed, speedY);
+
+        anim.SetInteger("Speed", (int)Mathf.Abs(x * 2));
+        if (x != 0) {
+            transform.localScale = new Vector3(x/Mathf.Abs(x), 1, 1);
         }
 
-        transform.position = new Vector3(posX, posY, 0);
+        Attacking("Attack", 1);
+
+        void Attacking(string attackName, int damage) {
+            if (Input.GetButtonDown(attackName)  == true) {
+                anim.SetTrigger(attackName);
+            }
+        }
     }
 }
