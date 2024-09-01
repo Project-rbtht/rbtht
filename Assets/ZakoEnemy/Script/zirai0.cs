ba808b521076b6c2d3594a9fa9cf72c4c9ac4dc3
nimparo-admin <ogretotem.game@gmail.com>
2024-09-01 20:41:49 +0900

    変なバグを修正

--- Assets/ZakoEnemy/Script/zirai0.cs
+++ Assets/ZakoEnemy/Script/zirai0.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class zirai : MonoBehaviour
+public class zirai0 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
