55a01206c6d94ad2105800552ca4d365ab8991ea
Shun-Miyamoto <147379522+Shun-Miyamoto@users.noreply.github.com>
2024-09-07 01:20:37 +0900

    ボス用にレーザーを作った

---
+++ Assets/Boss/BossBase.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBase : MonoBehaviour, IDamageable
+{
+    // Start is called before the first frame update
+    public int healthPoint = 2;
+    public int attack = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int value)//ƒ_ƒ[ƒWˆ—
+    {
+        UnityEngine.Debug.Log("on Damage");
+        healthPoint -= value;//HP‚ğvalue‚Ì‚Ô‚ñ‚¾‚¯Œ¸‚ç‚·
+        if (healthPoint <= 0)
+        {//HP‚ª0ˆÈ‰º‚É‚È‚Á‚½‚ç€
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Destroy(this.gameObject);
+        UnityEngine.Debug.Log("on Death");
+    }
+}
