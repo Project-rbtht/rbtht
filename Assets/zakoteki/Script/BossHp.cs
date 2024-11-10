b960f9e4d50c960b4e356254837c9625a9c17657
gumi93938 <gumi93938@gmail.com>
2024-11-10 19:13:58 +0900

    stage2 Boss

---
+++ Assets/zakoteki/Script/BossHp.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BossHp : MonoBehaviour, Idamagable
+{
+    public int hp = 1;
+    public int attackDamage = 2;
+
+    // シーン遷移時に移動するターゲットシーンの名前を指定
+    public string targetScene = "afterScene"; // 遷移先シーンの名前（適宜変更）
+
+    public void Damage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            Debug.Log("hp = " + hp);
+            // HPが0になった場合、指定したシーンに遷移
+            LoadScene();
+            Destroy(this.gameObject); // 敵を消滅させる
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Idamagable>().Damage(attackDamage);
+        }
+    }
+
+    // 指定したシーンに遷移するメソッド
+    private void LoadScene()
+    {
+        // FadeManagerを使って、シーンを指定して遷移
+        FadeManager.Instance.LoadScene(targetScene, 1.0f); // 1.0f はフェードの時間
+    }
+}
--- Assets/zakoteki/Script/BulletSpawner.cs
+++ Assets/zakoteki/Script/BulletSpawner.cs
@@ -7,6 +7,7 @@ public class BulletSpawner : MonoBehaviour
     public float bulletSpeed = 10f; // 弾の速度
     public float bulletLifeTime = 2f; // 弾の消滅までの時間
     public float spawnInterval = 1f; // 弾の生成間隔
+    public Vector2 bulletDirection = Vector2.up; // 弾を飛ばす方向（デフォルトは上方向）
 
     private void Start()
     {
@@ -31,10 +32,16 @@ public class BulletSpawner : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
-            rb.velocity = transform.up * bulletSpeed; // 上方向に飛ばす
+            rb.velocity = bulletDirection.normalized * bulletSpeed; // 指定方向に飛ばす
         }
 
         // 一定時間後に弾を消滅させる
         Destroy(bullet, bulletLifeTime);
+    }
+
+    // 弾の方向を外部から設定するメソッド
+    public void SetBulletDirection(Vector2 direction)
+    {
+        bulletDirection = direction.normalized; // 正規化して設定
     }
 }
